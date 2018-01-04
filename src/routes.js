d0dfd0968cb04718a0e4915d01a1dcd88142dd49
Tyler Smith <t@q1s.co>
2018-01-04 13:51:59 -0500

    Now with error handling
    for those smart sites that do not have pages.

--- src/routes.js
+++ src/routes.js
@@ -1,29 +1,35 @@
 const gql = require('graphql-tag')
 
 module.exports = async function (Vue, apolloClient) {
-  const adminPages = await apolloClient.query({
-    query: gql`
-      {
-        page{
-          nodes{
-            items{
-              nodes{
-                name
-                path
-                template
+let pageList = []
+  try {
+    apolloClient.query({
+      query: gql`
+        {
+          page{
+            nodes{
+              items{
+                nodes{
+                  name
+                  path
+                  template
+                }
               }
             }
           }
         }
+        `
+    })
+    pageList = adminPages.data.page.nodes[0].items.nodes.map(page => {
+      return {
+        path: page.path,
+        name: page.name,
+        component: Vue.component('defaultPage')
       }
-      `
-  })
-  const pageList = adminPages.data.page.nodes[0].items.nodes.map(page => {
-    return {
-      path: page.path,
-      name: page.name,
-      component: Vue.component('defaultPage')
-    }
-  })
-  return [...pageList]
+    })
+  } catch(e){
+    console.warn("!!THERE WAS AN ERROR IN RETRIEVING PAGES FROM THE SPECIFIED GRAPHQL CONNECTION", e)
+  } finally{
+    return [...pageList]
+  }
 }
